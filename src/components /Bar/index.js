e1e1ae97f87cf6bab923b06b41cde6ebfbe2508a
Donald Chromy <dgchromy@gmail.comglobal user.emailgit config user.emailgit config --global user.email dgchromy@gmail.com>
2020-12-11 21:20:34 -0800

    added components folder and bar folder

---
+++ src/components /Bar/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './style.css';
+import Search from '../Search/';
+import BtnGroup from '../ButtonGroup/';
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
+// This combines the filter search input and the filter button group of dropdowns and buttons. 
+function Bar({ inputValue, handleInputChange, resetBtn, sortBtn, children, handleSelectDropdown }) {
+    return (
+        <Row>
+            <Col md={4}>
+                <Search
+                    inputValue={inputValue}
+                    handleInputChange={handleInputChange} />
+            </Col>
+            <Col md={8} className="button-group">
+                <BtnGroup resetBtn={resetBtn} sortBtn={sortBtn} children={children} handleSelectDropdown={handleSelectDropdown} />
+            </Col>
+        </Row>
+
+    )
+}
+
+
+export default Bar;
