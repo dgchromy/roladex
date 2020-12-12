79142a40771325452182e2ca4bd13febcb0f922c
Donald Chromy <dgchromy@gmail.comglobal user.emailgit config user.emailgit config --global user.email dgchromy@gmail.com>
2020-12-11 22:08:15 -0800

    added to app.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,132 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import CardWrap from "./components/CardWrap";
+import Bar from "./components/Bar"
+import Header from "./components/Header";
+import GroupCard from "./components/GroupCard"
+import groupArray from "./group.json";
+import Main from "./components/Main"
 
 function App() {
+
+  const [group, setGroup] = useState(groupArray);
+
+  const [search, setSearch] = useState('');
+
+  const [warning, setWarning] = useState(false);
+
+  const [nameSort, setNameSort] = useState("AZ");
+
+  const handleInputChange = event => {
+    setSearch(event.target.value);
+  };
+
+  useEffect(() => {
+    if(!search) {
+      return setGroup(groupArray);
+    }
+
+    const results = group.filter(person => 
+      person.name.includes(search.toLowerCase())
+      );
+      setGroup(results);
+      setWarning(false);
+  }, [search])
+
+  const sortNames = () => {
+    switch (nameSort) {
+      case 'AZ':
+        setGroup([...group].sort((a, b) => (a.name > b.name) ? 1 : -1));
+        setNameSort("ZA")
+        break;
+      case 'ZA':
+        setGroup([...group].sort((a, b) => (a.name < b.name) ? 1 : -1));
+        setNameSort("AZ")
+        break;
+        default:
+          return;
+    }
+  }
+
+  const handleSelectDropdown = (eventKey) => {
+    switch (eventKey) {
+      case "Striker":
+        setGroup([...groupArray].filter(person =>
+          person.title.includes("Striker")
+        ));
+        break;
+      case "Center Mid":
+        setGroup([...groupArray].filter(person =>
+          person.title.includes("Center Mid")
+        ));
+        break;
+      case "Defender":
+        setGroup([...groupArray].filter(person =>
+          person.title.includes("Bassist")
+        ));
+        break;
+      case "Goalie":
+        setGroup([...groupArray].filter(person =>
+          person.title.includes("Goalie")
+        ));
+        break;
+      case "Seattle":
+        setGroup([...groupArray].filter(person =>
+          person.location.includes("Seattle")
+        ));
+        break;
+      case "Columbus":
+        setGroup([...groupArray].filter(person =>
+          person.location.includes("Columbus")
+        ));
+        break;
+      case "Portland":
+      setGroup([...groupArray].filter(person =>
+        person.location.includes("Portland")
+      ));
+      break;
+    default:
+      resetGroup();
+      return;       
+    }
+  }
+
+  const resetGroup = () => {
+    setGroup(groupArray);
+    setSearch("");
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Main>
+        <Bar
+          inputValue={search}
+          handleInputChange={handleInputChange}
+          resetBtn={resetGroup}
+          sortBtn={sortNames}
+          children={nameSort === "AZ" ? "Sort A–Z" : "Sort Z–A"}
+          handleSelectDropdown={handleSelectDropdown} />
+
+        {/* Validation */}
+        {warning === false ? null : <h4>Woops, please use letters only. Numbers or special characters won't display results.</h4>}
+        {group.length === 0 ? <h4>Looks like we don't have this team member. Please try a different name or hit "Reset".</h4> : null}
+        
+          <CardWrap>
+            {group.map(person => (
+              <GroupCard
+                key={person.id}
+                name={person.name}
+                title={person.title}
+                location={person.location}
+                phone={person.phone}
+                email={person.email}
+              />
+            ))}
+          </CardWrap>
+        
+      </Main>
+    </>
   );
 }
 
